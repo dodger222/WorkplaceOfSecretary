9ae3790d97131586bed26535a9983df806d614ed
Maxim <sipakovmaksim@gmail.com>
2019-05-19 18:08:14 +0300

    Add CRUD to Employee.

---
+++ WorkplaceOfSecretary/Controllers/EmployeeController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WorkplaceOfSecretary.Data;
+using WorkplaceOfSecretary.Models;
+
+namespace WorkplaceOfSecretary.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly WoSContext _context;
+
+        public EmployeeController(WoSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var woSContext = _context.Employees.Include(e => e.Degree).Include(e => e.Rank);
+            return View(await woSContext.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Degree)
+                .Include(e => e.Rank)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["DegreeID"] = new SelectList(_context.Degrees, "ID", "NameOfDegree");
+            ViewData["RankID"] = new SelectList(_context.Ranks, "ID", "NameOfRank");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(employee);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            ViewData["DegreeID"] = new SelectList(_context.Degrees, "ID", "NameOfDegree", employee.DegreeID);
+            ViewData["RankID"] = new SelectList(_context.Ranks, "ID", "NameOfRank", employee.RankID);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["DegreeID"] = new SelectList(_context.Degrees, "ID", "NameOfDegree", employee.DegreeID);
+            ViewData["RankID"] = new SelectList(_context.Ranks, "ID", "NameOfRank", employee.RankID);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstName,Patronymic,LastNameInGenitive,DegreeID,RankID")] Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
+                }
+            }
+            ViewData["DegreeID"] = new SelectList(_context.Degrees, "ID", "NameOfDegree", employee.DegreeID);
+            ViewData["RankID"] = new SelectList(_context.Ranks, "ID", "NameOfRank", employee.RankID);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Degree)
+                .Include(e => e.Rank)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            try
+            {
+                _context.Employees.Remove(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.)
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.ID == id);
+        }
+    }
+}
