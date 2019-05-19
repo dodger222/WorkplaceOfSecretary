4d6ea6ddd7c8c5124838c1f047a4681c3099f6be
Maxim <sipakovmaksim@gmail.com>
2019-05-19 17:19:10 +0300

    Add Employee model.

---
+++ WorkplaceOfSecretary/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkplaceOfSecretary.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public string LastNameInGenitive { get; set; }
+        [Column("IdDegree")]
+        public int DegreeID { get; set; }
+        [Column("IdRank")]
+        public int RankID { get; set; }
+
+        public Degree Degree { get; set; }
+        public Rank Rank { get; set; }
+
+        //public ICollection<Committee> CommitteesForChairperson { get; set; }
+        //public ICollection<Committee> CommitteesForSecretary { get; set; }
+        //public ICollection<Committee> CommitteesForMembers { get; set; }
+        //public ICollection<Protocol> Protocols { get; set; } 
+    }
+}
