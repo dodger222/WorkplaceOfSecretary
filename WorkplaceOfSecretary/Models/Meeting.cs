14939922555efe4bea31455f7ad066af5104f6e9
Maxim <sipakovmaksim@gmail.com>
2019-05-19 22:00:47 +0300

    Update Meeting model.

--- WorkplaceOfSecretary/Models/Meeting.cs
+++ WorkplaceOfSecretary/Models/Meeting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace WorkplaceOfSecretary.Models
         public int ID { get; set; }
         [Column("IdSEB")]
         public int SebID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
         public SEB Seb { get; set; }
